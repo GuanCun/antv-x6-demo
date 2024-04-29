4a5b99b58c2c2ec8ce147aee52d68c70ff4ebc2f
GuanCun <linguancun@163.com>
2024-04-29 23:48:24 +0800

    feat: x6 的基本配置

---
+++ packages/base-x6/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { onMounted } from "vue";
+import { Graph } from "@antv/x6";
+
+onMounted(() => {
+  const graph = new Graph({
+    container: document.getElementById("container"),
+    grid: true,
+  });
+
+  const source = graph.addNode({
+    x: 40,
+    y: 40,
+    width: 100,
+    height: 40,
+    label: "Hello",
+  });
+
+  const target = graph.addNode({
+    x: 160,
+    y: 160,
+    width: 100,
+    height: 40,
+    label: "World",
+  });
+
+  graph.addEdge({
+    source,
+    target,
+  });
+});
+</script>
+
+<template>
+  <div>
+    <div id="container" style="width: 500px; height: 500px"></div>
+  </div>
+</template>
+
+<style scoped></style>
